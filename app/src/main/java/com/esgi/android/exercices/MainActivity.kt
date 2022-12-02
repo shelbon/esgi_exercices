9fc782314cb9cf07106c7a560fc7705a8a21a42f
shelbon <mouclepatrick@protonmail.com>
2022-12-02 17:26:59 +0100

    code

--- app/src/main/java/com/esgi/android/exercices/MainActivity.kt
+++ app/src/main/java/com/esgi/android/exercices/MainActivity.kt
@@ -2,12 +2,12 @@ package com.esgi.android.exercices
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // TODO Changer cette ligne en fonction des layouts
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.empty_booking)
     }
 }
